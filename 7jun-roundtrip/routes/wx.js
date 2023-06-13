810bb8526a9b4754f43a4dfbfed7dde4bb3ba326
Perry <perry@crc-dot1x-nat-10-239-247-24.bu.edu>
2023-06-13 15:29:10 -0400

    adding a redis cache

--- 7jun-roundtrip/routes/wx.js
+++ 7jun-roundtrip/routes/wx.js
@@ -1,31 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const wxConfig = require('../config/wxConfig');
+const Redis = require('ioredis');
+const redis = new Redis();
 
 //matches http://localhost:3000/wx
 //
-router.get('/',  async(req, res, next) => {
+router.get('/', async (req, res, next) => {
     //get current weather from openweather.com
-    const rawWxInfo = await fetch(wxConfig.url + wxConfig.queryString)
-        .then(async response =>  await response.json())
-        .then(result => { console.log(result); return result;})
-        .then(result => {res.json(result)})
-        .catch(error => console.log('error', error));
-
-
-    //const rawWxInfo = await fetch(wxConfig.url + wxConfig.queryString)
-    //const wxInfo = await rawWxInfo.json();
-    //res.json(wxInfo);
+    //check cache
+    //
+    try {
+        let cachedData = await redis.get("wxInfo");
+        if (cachedData) {
+            let parsedData = await JSON.parse(cachedData);
+            console.log(`Cached: ${parsedData.current.temp}`);
+            res.json(cachedData)
+        }
+        let rawWxInfo = await fetch(wxConfig.url + wxConfig.queryString)
+        let parsedData = await rawWxInfo.json();
+        console.table(`Live temp: ${parsedData.current.temp}`);
+        let response = await redis.set("wxInfo", parsedData, "EX", 5);
+        res.json(parsedData);
+    } catch (err) {
+        console.log(`Error: ${err}`);
+    }
 })
-
 module.exports = router; //the export makes router visible to any file that require()s it
-
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
-
-// fetch("https://api.openweathermap.org/data/3.0/onecall?lat=42.4&lon=-71.0&exclude=minutely,hourly,daily,alerts&appid=6d13b989d2175d527ad89638956aec97&units=imperial", requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
